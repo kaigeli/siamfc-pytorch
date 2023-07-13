959a3458fa54df50dee9eb68a4bbb53ee65a72be
Kaige Li <840745490@qq.com>
2023-07-13 11:52:03 +0800

    siamfc: add parse_arguments for video path and model path.
    Signed-off-by: Kaige Li <840745490@qq.com>

--- tools/track_video.py
+++ tools/track_video.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+import argparse
 import os
 import glob
 import numpy as np
@@ -8,9 +9,18 @@ from siamfc import TrackerSiamFC
 
 import cv2
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Training script")
+    # 替换成你的test视频文件路径
+    parser.add_argument('-v', '--video_path', default='~/2_dataset/football_my_dataset/IMG_0773.MOV', type=str,
+                        help="you will track the video path.")
+    parser.add_argument('-m', '--model_path', default='./pretrained/siamfc_alexnet_e50.pth', type=str,
+                        help="the pretrained model path.")
+    args = parser.parse_args()
+    return args
+
 def mouse_callback(event, x, y, flags, param):
     global box_start, drawing_box, box_coordinates
-
     if event == cv2.EVENT_LBUTTONDOWN:
         box_start = (x, y)
         drawing_box = True
@@ -19,27 +29,22 @@ def mouse_callback(event, x, y, flags, param):
         img = frame.copy()
         cv2.rectangle(img, box_start, (x, y), (255, 0, 0), 2)
         cv2.imshow("Frame", img)
-        # 获取框的坐标
+        # 获取框的坐标[x, y, w, h]
         box_coordinates = [box_start[0], box_start[1], x-box_start[0], y-box_start[1]]
         print("Box Coordinates:", box_coordinates)
-    elif drawing_box == True:
+    elif event == cv2.EVENT_MOUSEMOVE and flags==cv2.EVENT_FLAG_LBUTTON:
         img = frame.copy()
         cv2.rectangle(img, box_start, (x, y), (255, 0, 0), 2)
         cv2.imshow("Frame", img)
 
-if __name__ == '__main__':
-    # 替换成你的test视频文件路径
-    video_path = "~/2_dataset/football_my_dataset/IMG_0773.MOV"
-    video_path = os.path.expanduser(video_path)
-    
+def get_box(video_path):
+    global frame
+
     cap = cv2.VideoCapture(video_path)
     ret, frame = cap.read()
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     if ret:
-        drawing_box = False
-        box_start = None
-        
         cv2.namedWindow("Frame", cv2.WINDOW_NORMAL)
         cv2.resizeWindow('Frame', width, height)
         cv2.imshow('Frame', frame)
@@ -53,11 +58,11 @@ if __name__ == '__main__':
         cv2.destroyAllWindows()
     else:
         print("Failed to read video file.")
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    video_path = os.path.expanduser(args.video_path)
+    get_box(video_path)
     
-    #if use groundtruth_rect.txt cancel next two lines
-    #seq_dir = os.path.expanduser('~/2_dataset/football_my_dataset/')
-    #anno = np.loadtxt(seq_dir + 'groundtruth_rect.txt', delimiter=',')
-    
-    net_path = 'pretrained/siamfc_alexnet_e50.pth'
-    tracker = TrackerSiamFC(net_path=net_path)
+    tracker = TrackerSiamFC(net_path=args.model_path)
     tracker.track_video(video_path, box_coordinates, visualize=True)
