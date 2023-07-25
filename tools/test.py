9caa3eab123d3ef5f76827bb1f155a728b39cfc7
Kaige Li <840745490@qq.com>
2023-07-25 23:27:19 +0800

    add got10k for support my dataset.
    my dataset: football
    
    Signed-off-by: Kaige Li <840745490@qq.com>

--- tools/test.py
+++ tools/test.py
@@ -7,16 +7,18 @@ from siamfc import TrackerSiamFC
 
 
 if __name__ == '__main__':
-    net_path = 'pretrained/siamfc_alexnet_e50.pth'
+    net_path = 'pretrained/siamfc_alexnet_e200.pth'
     tracker = TrackerSiamFC(net_path=net_path)
-    root_dir = os.path.expanduser('~/2_dataset/OTB100')
-    e = ExperimentOTB(root_dir, version=2015)
+    root_dir = os.path.expanduser('~/2_dataset/football_my_dataset/second/football')
+    #root_dir = os.path.expanduser('~/2_dataset/OTB100')
+    e = ExperimentOTB(root_dir, version='football')
     # 2013: __otb13_seqs,
     # 2015: __tb100_seqs,
     # 'otb2013': __otb13_seqs,
     # 'otb2015': __tb100_seqs,
     # 'tb50': __tb50_seqs,
     # 'tb100': __tb100_seqs
+    # 'football':__football_seqs
     # e = ExperimentOTB(root_dir, version='tb50')
     # e = ExperimentUAV123(root_dir, version='tb50') to do
     e.run(tracker)
