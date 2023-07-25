9774c2b095762c476d290d82ba727e14403ac853
Kaige Li <840745490@qq.com>
2023-07-25 23:26:18 +0800

    add code comments.
    Signed-off-by: Kaige Li <840745490@qq.com>

--- siamfc/heads.py
+++ siamfc/heads.py
@@ -18,9 +18,16 @@ class SiamFC(nn.Module):
     
     def _fast_xcorr(self, z, x):
         # fast cross correlation
+        # x.shape = torch.Size([3, 256, 22, 22]), 3是因为scale_num = 3
+        # z.shape = torch.Size([1, 256, 6, 6])
         nz = z.size(0)
+        # nz = 1
         nx, c, h, w = x.size()
         x = x.view(-1, nz * c, h, w)
+        # x.shape = torch.Size([3, 256, 22, 22])
         out = F.conv2d(x, z, groups=nz)
+        # out.shape = torch.Size([3, 1, 17, 17])
         out = out.view(nx, -1, out.size(-2), out.size(-1))
+        # print(out.shape)
+        # out.shape = torch.Size([3, 1, 17, 17])
         return out
